f603691ffd9ec2e398f28085f6c2d67a00985735
Gokusagar <103039528+Gokusagar@users.noreply.github.com>
2022-04-19 19:01:36 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def mydeploy(jobname,ip,name)
 {
   "sh scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${name}"
 }
+def mytest()
+{
+  sh 'java -jar testing.jar'
+}
