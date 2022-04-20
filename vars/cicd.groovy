4a58276eb84f816cc15864ba3bf4cbd16d7570a7
Gokusagar <103039528+Gokusagar@users.noreply.github.com>
2022-04-20 15:49:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,8 @@
-def mylab(repo)
+def mygit(repo)
 {
-git "${repo}"
+  "git ${repo}"
 }
 def mybuild()
 {
-sh 'mvn package'
-}
-def mydeploy(jobname,ip,name)
-{
-  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${name}"
-}
-def mytest()
-{
-  sh 'java -jar testing.jar'
+  sh 'mvn package'
 }
