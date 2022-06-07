56e5bc20f1fc8bb864c3d50bc5c4367a38bb118c
Gokusagar <103039528+Gokusagar@users.noreply.github.com>
2022-06-07 16:29:34 +0530

    Update jenkins.groovy

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,1 +1,11 @@
-def mygit(
+def mygit(repo)
+{
+  git"${repo}"
+}
+def mybuild()
+{
+  sh 'mvn package'
+}
+def mydeploy()
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/"${name}"/webapp/target/webapp.war ubuntu@i"${ip}":/var/lib/tomcat9/webapps/"${context}"'
